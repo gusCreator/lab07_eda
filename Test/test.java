ce18d21c2f4481cd5e9adc929687a9c9bbf58547
nuevo_63 <ajrahuacso@gmail.com>
2024-07-04 09:37:10 -0500

    Creacion del test

---
+++ Test/test.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class test {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("Steven");
+        trie.insert("Palabra");
+
+        System.out.println(trie.search("Steven")); // true
+        System.out.println(trie.search("Palabra")); // true
+        System.out.println(trie.search("Steve"));  // false
+
+        trie.delete("Palabra");
+        System.out.println(trie.search("Palabra")); // false
+    }
+}
