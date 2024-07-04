41a6ca20399c66e3fd10182232a1481f872ec565
gusCreator <lgsc21211@gmail.com>
2024-07-04 11:00:13 -0500

    Se crea una heap para obtener los máximos

---
+++ heap/HeapMax.java
@@ -0,0 +1,62 @@
+package heap;
+
+public class HeapMax<E extends Comparable<E>> {
+  
+  private E[] arr;
+  private int size;
+
+  @SuppressWarnings("unchecked")
+  public HeapMax(int initialSize) {
+    this.size = 0;
+    this.arr = (E[]) new Object();
+  }
+
+  public void insert(E x) {
+    arr[this.size] = x;
+    for(int i = this.size++; i >= 0;){
+      if(arr[i].compareTo(arr[(i - 1) / 2]) > 0){
+        E temp = arr[i];
+        arr[i] = arr[(i - 1) / 2];
+        arr[(i - 1) / 2] = temp;
+        i = (i - 1) / 2;
+      }else{
+        break;
+      }
+    }
+  }
+
+  public E extract() {
+    if(isEmpty()) return null;
+    E temp = arr[0];
+    arr[0] = arr[this.size - 1];
+    for(int i = 0; i * 2 + 1 < this.size && i * 2 + 2 < this.size;){
+      if(arr[i * 2 + 1].compareTo(arr[i * 2 + 2]) < 0) {
+        E tempe = arr[i];
+        arr[i] = arr[i * 2 + 1];
+        arr[i * 2 + 1] = tempe;
+        i = i * 2 + 1;
+      }else{
+        E tempe = arr[i];
+        arr[i] = arr[i * 2 + 2];
+        arr[i * 2 + 2] = tempe;
+        i = i * 2 + 2;
+      }
+    }
+    return temp;
+  }
+
+  public boolean isEmpty() {
+    return this.size == 0;
+  }
+
+  public String toString() {
+    if(isEmpty()) return "[]";
+    String s = "[";
+    for(E e : this.arr){
+      s += e.toString() + ", ";
+    }
+    return s + "]";
+  }
+
+
+}
