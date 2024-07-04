f670f21d69ced073dddbcb3d623dfb275db0414c
gusCreator <lgsc21211@gmail.com>
2024-07-04 10:16:01 -0500

    Se añade un método `searchNode` que devuelve el nodo de acuerdo a la palabra ingresaba. Es utilizado por los métodos `getRepetitions` y `search` para devolver sus respectivas respuestas de acuerdo a su signatura.

--- trie/Trie.java
+++ trie/Trie.java
@@ -3,44 +3,55 @@ package trie;
 import java.util.HashMap;
 
 public class Trie {
-    private Node root;
+  private Node root;
 
-    public Trie() {
-        root = new Node();
+  public Trie() {
+    root = new Node();
+  }
+
+  public void insert(String key) {
+    Node current = root;
+    for (char c : key.toCharArray()) {
+      current = current.children.computeIfAbsent(c, cha -> new Node());
     }
+    current.repetitions++;
+    current.isEndOfWord = true;
+  }
 
-    public void insert(String key) {
-      Node current = root;
-      for(char c :  key.toCharArray()){
-        current = current.children.computeIfAbsent(c, cha -> new Node());
+  public boolean search(String word) {
+    Node node = this.searchNode(word);
+    if(node == null) return false;
+    return node.isEndOfWord;
+  }
+
+  public int getRepetitions(String key) {
+    Node node = this.searchNode(key);
+    if(node == null) return 0;
+    return node.repetitions;
+  }
+
+  private Node searchNode(String key) {
+    Node current = root;
+    for (int i = 0; i < key.length(); i++) {
+      char ch = key.charAt(i);
+      Node node = current.children.get(ch);
+      if (node == null) {
+        return null;
       }
-      current.isEndOfWord = true;
+      current = node;
     }
+    return current;
+  }
 
-    public boolean search(String word) {
-        Node current = root;
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            Node node = current.children.get(ch);
-            if (node == null) {
-                return false;
-            }
-            current = node;
-        }
-        return current.isEndOfWord;
+  private static class Node {
+    HashMap<Character, Node> children;
+    boolean isEndOfWord;
+    int repetitions;
+
+    public Node() {
+      this.children = new HashMap<Character, Node>();
+      this.isEndOfWord = false;
+      this.repetitions = 0;
     }
-
-    public void delete(String key) {
-
-    }
-
-    private static class Node {
-      HashMap<Character, Node> children;
-      boolean isEndOfWord;
-  
-      public Node() {
-          children = new HashMap<Character, Node>();
-          isEndOfWord = false;
-      }
   }
 }
